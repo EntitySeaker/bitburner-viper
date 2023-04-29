205f577e1bd9028bbe65c2c1151bcdaf25ceabea
thinkpadX1 <thinkpadX1@example.com>
2023-04-30 01:24:30 +0200

    dev

--- main/main.js
+++ main/main.js
@@ -4,10 +4,10 @@ export async function main(ns) {
 	while (true){
 
 		var input = await user_input("Console: ", "red")
-		ns.tprintf("\u001b[31m"+"Console: "+"\u001b[32m"+input)
+		ns.tprintf("\u001b[31m" + "Console: " + "\u001b[32m" + input)
 		
 		if (!(input in commands)) {
-			ns.tprintf("Command "+input+' does not exist, type "help" for help.')
+			ns.tprintf("Command " + input + ' does not exist, type "help" for help.')
 			continue
 		}
 
